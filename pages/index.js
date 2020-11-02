3257438e5833010467cbf945e3c284398cadf235
victorpoliushko <milleniiumkb@gmail.com>
2020-11-02 16:13:32 +0200

    SEO and meta

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Index() {
-
   return (
     <>
+    <Head>
+      <title>Next Title</title>
+      <meta name="keywords" content="next,javascript,react,nextjs" />
+      <meta name="description" content="Next.js project by Viktor Poliushko" />
+      <meta charSet='utf-8' />
+    </Head>
     <h1>Hello Next.js</h1>
     <p><Link href={'/about'}><a>About</a></Link></p>
     <p><Link href={'/posts'}><a>Posts</a></Link></p>
--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,12 @@
+import Head from 'next/head';
+
 export default function Posts() {
   return(
-    <h1>Posts page</h1>
+    <>
+      <Head>
+        <title>Posts page | Next course</title>
+      </Head>
+      <h1>Posts page</h1>
+    </>
   );
 }
