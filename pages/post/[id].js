acb22d86437d1ba0656adc867d162b35d8d5c35a
victorpoliushko <milleniiumkb@gmail.com>
2020-11-16 19:22:27 +0200

    getServerSideProps

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -39,6 +39,9 @@ export default function Post({ post: serverPost }) {
 }
 
 Post.getInitialProps = async ({ query, req }) => {
+  if(!req) {
+    return { posts: null }
+  }
   const response = await fetch(`http://localhost:4200/posts/${query.id}`);
   const post = await response.json();
   return { post }
